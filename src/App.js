ba53279ae77214b6138feb3baf1346eac109ed18
AKA717 <alwinaji717@gmail.com>
2023-04-16 17:07:49 +0530

    first one

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Navbar  from './navbar/navbar';
+import Landing from './landing/landing';
+import './App.css';
+
+function App() {
+  return (
+   <div class="main">
+    <Navbar/>
+    <Landing/>
+   </div>
+  );
+}
+
+export default App;
---
+++ src/navbar/navbar.js
@@ -0,0 +1,19 @@
+import './navbar.css'
+
+const Navbar = () => {
+    return (
+        <div class='header'>
+            <div class="logo">
+                LOGO
+            </div>
+        <div class="options">
+            <div class="items">
+            <a href='' >ABOUT</a>
+            <a href='' >CONTACTS</a>
+            </div>
+      </div>
+    </div>
+    );
+}
+
+export default Navbar;
